ccb16a162edaf9d97a1dab849c8f737da1d00095
jpcamba <camba.jannieca@gmail.com>
2019-05-16 23:33:28 +0800

    Write values to csv

--- airscraper/pipelines.py
+++ airscraper/pipelines.py
@@ -1,6 +1,22 @@
+import csv
+
 class SearchPipeline(object):
     def process_item(self, item, spider):
         item['flightNumber'] = item['flightNumber'].strip()
         item['fare'] = float(item['fare'].strip().strip('PHP').replace(',', ''))
         print item
+        return item
+
+class CsvWriterPipeline(object):
+    def open_spider(self, spider):
+        self.file = open('items.csv', 'wb')
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        csvWriter = csv.writer(self.file, delimiter= ',')
+        flightNumber = item['flightNumber'].strip()
+        fare = float(item['fare'].strip().strip('PHP').replace(',', ''))
+        csvWriter.writerow([flightNumber, fare])
         return item
