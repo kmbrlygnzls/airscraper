f69db302fbf62aefc9dd903b598793fcca218757
Kimberly Ann Gonzales <kgonzales@factset.com>
2019-05-17 13:04:22 +0800

    fix bugs

--- airscraper/app.py
+++ airscraper/app.py
@@ -9,10 +9,10 @@ from util import Date
 
 def main():
 	option = sys.argv[1]
-	if sys.argv[2]: budget = sys.argv[2]
-	if sys.argv[3]: sdate = sys.argv[3]
-	if sys.argv[4]: edate = sys.argv[4]
-	if sys.argv[5 ]:duration = sys.argv[5]
+	if len(sys.argv)>=3: budget = sys.argv[2]
+	if len(sys.argv)>=4: sdate = sys.argv[3]
+	if len(sys.argv)>=5: edate = sys.argv[4]
+	if len(sys.argv)>=6: duration = sys.argv[5]
 
 	if option == '4':
 		o_df = pd.read_csv('depart.csv')
