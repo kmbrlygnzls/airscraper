7f2331a844b72e62a9daf38c6067426efa128117
Kimberly Ann Gonzales <kgonzales@factset.com>
2019-05-17 12:54:23 +0800

    working

--- airscraper/app.py
+++ airscraper/app.py
@@ -9,7 +9,10 @@ from util import Date
 
 def main():
 	option = sys.argv[1]
-	budget = int(sys.argv[2])
+	if sys.argv[2]: budget = sys.argv[2]
+	if sys.argv[3]: sdate = sys.argv[3]
+	if sys.argv[4]: edate = sys.argv[4]
+	if sys.argv[5 ]:duration = sys.argv[5]
 
 	if option == '4':
 		o_df = pd.read_csv('depart.csv')
@@ -25,40 +28,51 @@ def main():
 		sum_data = {
 			'DateRange' : date_combo,
 			'Flights' : flightnum_combo,
-			'RTFare' : fare_sum
+			'RTFare' : fare_sum,
+			'Route' : route_combo
 		}
-		sum_df = pd.DataFrame(sum_data, columns=['DateRange', 'Flights', 'RTFare'])
+		sum_df = pd.DataFrame(sum_data, columns=['DateRange', 'Flights', 'RTFare', 'Route'])
 		print(sum_df.sort_values(by='RTFare'))
 
 	elif option == '6':
-		sum_df = pd.DataFrame(columns=['DateRange', 'Flights', 'RTFare'])
+		sum_df = pd.DataFrame(columns=['DateRange', 'Flights', 'RTFare', 'Route'])
+		sdate = Date.parseDate(sdate)
+		edate = Date.parseDate(edate)
 		start_date = sdate
-		delta = datetime.timedelta(days=duration)
+		delta = datetime.timedelta(days=int(duration))
 		end_date = start_date + delta
 		while (start_date + delta) <= edate:
 			end_date = start_date + delta
 
 			o_df = pd.read_csv('depart.csv')
+			print(start_date)
 			o_df = o_df[o_df.Date == str(start_date)]
 
-			d_df = pd.read_csv('depart.csv')
+			d_df = pd.read_csv('return.csv')
+			print(end_date)
 			d_df = d_df[d_df.Date == str(end_date)]
 
 			o_df.sort_values(by='Fare', inplace=True)
+			print(o_df)
 			d_df.sort_values(by='Fare', inplace=True)
+			print(d_df)
 
 			date_combo = [str(d1) + " to " + str(d2) for d1 in o_df['Date'] for d2 in d_df['Date']]
 			flightnum_combo = [fn1 + " and " + fn2 for fn1 in o_df['FlightNumber'] for fn2 in d_df['FlightNumber']]
+			route_combo = [r1 for r1 in o_df['Route'] for r2 in d_df['Route']]
 			fare_sum = [f1+f2 for f1 in o_df['Fare'] for f2 in d_df['Fare']]
+
 
 			data = {
 				'DateRange' : date_combo,
 				'Flights' : flightnum_combo,
-				'RTFare' : fare_sum
+				'RTFare' : fare_sum,
+				'Route' : route_combo
 			}
-			df = pd.DataFrame(data, columns=['DateRange', 'Flights', 'RTFare'])
+			df = pd.DataFrame(data, columns=['DateRange', 'Flights', 'RTFare', 'Route'])
 			sum_df = sum_df.append(df, ignore_index = True)
 			start_date += datetime.timedelta(days=1)
+		sum_df = sum_df[sum_df.RTFare <= int(budget)]
 		print(sum_df.sort_values(by='RTFare'))
 
 	elif option in ['1', '2', '3']:
@@ -68,10 +82,8 @@ def main():
 	elif option == '5' and budget:
 		print("option 5")
 		o_df = pd.read_csv('depart.csv')
-		o_df = o_df[o_df.Fare <= budget]
-		print(o_df.sort_values(by='Fare'))
-	
-
+		o_df = o_df[o_df.RTFare <= int(budget)]
+		print(o_df.sort_values(by='RTFare'))
 
 if __name__ == "__main__":
 	main()
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import os
 
-def main():
-    print('1 Cheapest one-way fare for given route and date')
-    print('2 Cheapest one-way fare for given route and date range')
-    print('3 Cheapest one-way fare for multiple destinations and date')
-    print('4 Cheapest roundtrip fare for given route and date range')
-    print('5 One-way fares that match the budget for a date range')
-    print('6 Roundtrip fares that match the budget for a date range')
-    option = input('Option: ')
-    print('')
+print('1 Cheapest one-way fare for given route and date')
+print('2 Cheapest one-way fare for given route and date range')
+print('3 Cheapest one-way fare for multiple destinations and date')
+print('4 Cheapest roundtrip fare for given route and date range')
+print('5 One-way fares that match the budget for a date range')
+print('6 Roundtrip fares that match the budget for a date range')
+option = '0'
+option = input('Option: ')
+print('')
 
-    parameters = ''
-    budget = 100000
+parameters = ''
+budget = 100000
+parse_params = ''
+
+def main():
     if option == '1':
         parameters = oneWaySingleDate()
     elif option == '2':
@@ -25,60 +28,73 @@ def main():
     elif option == '6':
         parameters = cheapestByDateRange()
     scrape(parameters)
-    get_results(option, budget)
+    get_results()
 
 def oneWaySingleDate():
+    global parse_params
     origin = input('Origin (ex. MNL): ')
     destination = input('Destination (ex. HKG): ')
     departureDate = input('Departure Date (ex. 2019-01-01): ')
     parameters = '-a option=oneWaySingleDate -a origin=' + origin + ' -a destination=' + destination + ' -a departureDate=' + departureDate
+    parse_params = "{}".format(option)
     return parameters
 
 def oneWayDateRange():
+    global parse_params
     origin = input('Origin (ex. MNL): ')
     destination = input('Destination (ex. HKG): ')
     departureDateFrom = input('Departure Date From (ex. 2019-01-01): ')
     departureDateTo = input('Departure Date To (ex. 2019-01-07): ')
     parameters = '-a option=oneWayDateRange -a origin=' + origin + ' -a destination=' + destination + ' -a departureDateFrom=' + departureDateFrom + ' -a departureDateTo=' + departureDateTo
+    parse_params = "{}".format(option)
     return parameters
 
 def multipleSingleDate():
+    global parse_params
     origin = input('Origin (ex. MNL): ')
     destinations = input('Comma-delimited Destinations (ex. HKG,ICN,NRT): ')
     departureDate = input('Departure Date (ex. 2019-01-01): ')
     parameters = '-a option=multipleSingleDate -a origin=' + origin + ' -a destinations=' + destinations + ' -a departureDate=' + departureDate
+    parse_params = "{}".format(option)
     return parameters
 
 def roundTripDateRange():
+    global parse_params
     origin = input('Origin (ex. MNL): ')
     destination = input('Destination (ex. HKG): ')
     departureDate = input('Departure Date (ex. 2019-01-01): ')
     returnDate = input('Return Date (ex. 2019-01-02): ')
     parameters = '-a option=roundTripDateRange -a origin=' + origin + ' -a destination=' + destination + ' -a departureDate=' + departureDate + ' -a returnDate=' + returnDate
+    parse_params = "{}".format(option)
     return parameters
 
 def cheapestByDateRange():
+    global parse_params
     origin = input('Origin (ex. MNL): ')
     budget = input('Budget in PHP (ex. 5000): ')
     startDate = input('Date Range Start (ex. 2019-01-01): ')
     endDate = input('Date Range End (ex. 2019-02-01): ')
     duration = input('Duration of Trip in Days: ')
     parameters = '-a option=cheapestByDateRange -a origin=' + origin + ' -a budget=' + budget + ' -a startDate=' + startDate + ' -a endDate=' + endDate + ' -a duration=' + duration
+    parse_params = "{} {} {} {} {}".format(option, budget, startDate, endDate, duration)
     return parameters
 
 def oneWayByBudget():
+    global parse_params
     origin = input('Origin (ex. MNL): ')
     budget = input('Budget in PHP (ex. 5000): ')
     departureDateFrom = input('Departure Date From (ex. 2019-01-01): ')
     departureDateTo = input('Departure Date To (ex. 2019-01-07): ')
     parameters = '-a option=oneWayByBudget -a origin=' + origin + ' -a budget=' + budget + ' -a departureDateFrom=' + departureDateFrom + ' -a departureDateTo=' + departureDateTo
+    parse_params = "{} {}".format(option, budget)
     return parameters
 
 def scrape(parameters):
-    os.system('scrapy runspider airscraper/spiders/search.py ' + parameters + ' --nolog')
+    os.system('scrapy runspider airscraper/spiders/search.py ' + parameters)
 
-def get_results(option, budget):
-    call_script_cmd = "python airscraper/app.py {} {}".format(option, budget)
+def get_results():
+    call_script_cmd = "python airscraper/app.py " + parse_params
+    print(call_script_cmd)
     os.system(call_script_cmd)
 
 main()
