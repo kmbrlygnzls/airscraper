bf20d9867c11d0dd2ab653fb56118c92cd147f9a
Kimberly Ann Gonzales <kgonzales@factset.com>
2019-05-17 08:44:46 +0800

    integrating changes for option 2

--- airscraper/app.py
+++ airscraper/app.py
@@ -1,28 +1,34 @@
 import collections
 import pandas as pd
 import itertools
+import sys
 
 
 def main():
-	o_df = pd.read_csv('origin_data.csv')
-	d_df = pd.read_csv('dest_data.csv')
+	option = sys.argv[0]
 
-	o_df.sort_values(by='Fare', inplace=True)
-	d_df.sort_values(by='Fare', inplace=True)
+	if option != "4":
+		o_df = pd.read_csv('depart.csv')
+		print(o_df.sort_values(by='Fare'))
+	else:
+		o_df = pd.read_csv('depart.csv')
+		d_df = pd.read_csv('return.csv')
 
-	date_combo = [str(d1) + " to " + str(d2) for d1 in o_df['Date'] for d2 in d_df['Date']]
-	flightnum_combo = [fn1 + " and " + fn2 for fn1 in o_df['FlightNumber'] for fn2 in d_df['FlightNumber']]
-	fare_sum = [f1+f2 for f1 in o_df['Fare'] for f2 in d_df['Fare']]
+		o_df.sort_values(by='Fare', inplace=True)
+		d_df.sort_values(by='Fare', inplace=True)
 
-	sum_data = {
-		'DateRange' : date_combo,
-		'Flights' : flightnum_combo,
-		'RTFare' : fare_sum
-	}
-	sum_df = pd.DataFrame(sum_data, columns=['DateRange', 'Flights', 'RTFare'])
+		date_combo = [str(d1) + " to " + str(d2) for d1 in o_df['Date'] for d2 in d_df['Date']]
+		flightnum_combo = [fn1 + " and " + fn2 for fn1 in o_df['FlightNumber'] for fn2 in d_df['FlightNumber']]
+		fare_sum = [f1+f2 for f1 in o_df['Fare'] for f2 in d_df['Fare']]
 
-	print(sum_df.sort_values(by='RTFare'))
-	sum_df.to_json(r'C:\Users\kgonzales\airscraper\airscraper\sorted_data.json', orient='records')
+		sum_data = {
+			'DateRange' : date_combo,
+			'Flights' : flightnum_combo,
+			'RTFare' : fare_sum
+		}
+		sum_df = pd.DataFrame(sum_data, columns=['DateRange', 'Flights', 'RTFare'])
+
+		print(sum_df.sort_values(by='RTFare'))
 
 
 if __name__ == "__main__":
