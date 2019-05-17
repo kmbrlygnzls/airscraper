ee857f74269b568120062c1cddb3c9acbd9c73bc
Kimberly Ann Gonzales <kgonzales@factset.com>
2019-05-17 10:53:53 +0800

    working with date duration round trip

--- airscraper/app.py
+++ airscraper/app.py
@@ -2,17 +2,31 @@ import collections
 import pandas as pd
 import itertools
 import sys
+import datetime
+from datetime import timedelta,date
+from util import Date
 
 
 def main():
-	option = sys.argv[0]
+	f = 1
+	sdate = Date.parseDate("2019-05-25")
+	edate = Date.parseDate("2019-05-30")
+	duration = 3
 
-	if option != "4":
-		o_df = pd.read_csv('depart.csv')
-		print(o_df.sort_values(by='Fare'))
-	else:
-		o_df = pd.read_csv('depart.csv')
-		d_df = pd.read_csv('return.csv')
+	sum_df = pd.DataFrame(columns=['DateRange', 'Flights', 'RTFare'])
+
+	start_date = sdate
+	delta = datetime.timedelta(days=duration)
+	end_date = start_date + delta
+	while (start_date + delta) <= edate:
+		end_date = start_date + delta
+
+		print("for {} to {}.....".format(start_date, end_date))
+		o_df = pd.read_csv('origin_data.csv')
+		o_df = o_df[o_df.Date == str(start_date)]
+
+		d_df = pd.read_csv('dest_data.csv')
+		d_df = d_df[d_df.Date == str(end_date)]
 
 		o_df.sort_values(by='Fare', inplace=True)
 		d_df.sort_values(by='Fare', inplace=True)
@@ -21,14 +35,16 @@ def main():
 		flightnum_combo = [fn1 + " and " + fn2 for fn1 in o_df['FlightNumber'] for fn2 in d_df['FlightNumber']]
 		fare_sum = [f1+f2 for f1 in o_df['Fare'] for f2 in d_df['Fare']]
 
-		sum_data = {
+		data = {
 			'DateRange' : date_combo,
 			'Flights' : flightnum_combo,
 			'RTFare' : fare_sum
 		}
-		sum_df = pd.DataFrame(sum_data, columns=['DateRange', 'Flights', 'RTFare'])
-
-		print(sum_df.sort_values(by='RTFare'))
+		df = pd.DataFrame(data, columns=['DateRange', 'Flights', 'RTFare'])
+		sum_df = sum_df.append(df, ignore_index = True)
+		start_date += datetime.timedelta(days=1)
+	
+	print(sum_df.sort_values(by='RTFare'))
 
 
 if __name__ == "__main__":
