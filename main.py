5ab6e849e8ed5545163122944106c911203e91b7
jpcamba <camba.jannieca@gmail.com>
2019-05-17 13:04:40 +0800

    Less nooby CLI. Fix return destination

--- main.py
+++ main.py
@@ -1,30 +1,51 @@
 import os
 
 def main():
-    print('1 Cheapest one-way fare for given route and date')
-    print('2 Cheapest one-way fare for given route and date range')
-    print('3 Cheapest one-way fare for multiple destinations and date')
-    print('4 Cheapest roundtrip fare for given route and date range')
-    print('5 One-way fares that match the budget for a date range')
-    print('6 Roundtrip fares that match the budget for a date range')
-    option = input('Option: ')
-    print('')
+    airplane = "\n\n========================================   AirScraper   ===========================================\n\n"
+    airplane += "                                               |\n"
+    airplane += "                                               |\n"
+    airplane += "                                               |\n"
+    airplane += "                                             .-'-.\n"
+    airplane += "                                            ' ___ '\n"
+    airplane += "                                  ---------'  .-.  '---------\n"
+    airplane += "                   ________________________'  '-'  '_________________________\n"
+    airplane += "                   ''''''-|---|--/    \==][^',_m_,'^][==/    \--|---|-''''''\n"
+    airplane += "                                 \    /  ||/   H   \||  \    /\n"
+    airplane += "                                  '--'   OO   O|O   OO   '--'\n\n\n"
+    airplane += 'Know the cheapest one-way and roundtrip fares for a given date, destination, budget, and more!\n'
+    airplane += 'This scrapes data from the Cebu Pacific Airlines website, and built with Scrapy and Pandas (Python)\n\n'
+    airplane += '===================================================================================================\n'
+    input(airplane)
 
-    parameters = ''
-    if option == '1':
-        parameters = oneWaySingleDate()
-    elif option == '2':
-        parameters = oneWayDateRange()
-    elif option == '3':
-        parameters = multipleSingleDate()
-    elif option == '4':
-        parameters = roundTripDateRange()
-    elif option == '5':
-        parameters = oneWayByBudget()
-    elif option == '6':
-        parameters = cheapestByDateRange()
-    scrape(parameters)
-    get_results(option)
+    while True:
+        print('\nSelect an option below:')
+        print('1 Cheapest one-way fare for given route and date')
+        print('2 Cheapest one-way fare for given route and date range')
+        print('3 Cheapest one-way fare for multiple destinations and date')
+        print('4 Cheapest roundtrip fare for given route and date range')
+        print('5 One-way fares that match the budget for a date range')
+        print('6 Roundtrip fares that match the budget for a date range')
+        option = input('Option: ')
+        print('')
+
+        parameters = ''
+        if option == '1':
+            parameters = oneWaySingleDate()
+        elif option == '2':
+            parameters = oneWayDateRange()
+        elif option == '3':
+            parameters = multipleSingleDate()
+        elif option == '4':
+            parameters = roundTripDateRange()
+        elif option == '5':
+            parameters = oneWayByBudget()
+        elif option == '6':
+            parameters = cheapestByDateRange()
+        elif option == 'exit':
+            exit()
+        scrape(parameters)
+        get_results(option)
+        input('\nPress Enter for next selection')
 
 def oneWaySingleDate():
     origin = input('Origin (ex. MNL): ')
@@ -74,7 +95,7 @@ def oneWayByBudget():
     return parameters
 
 def scrape(parameters):
-    os.system('scrapy runspider airscraper/spiders/search.py ' + parameters + ' --nolog')
+    os.system('scrapy runspider airscraper/spiders/search.py ' + parameters)
 
 def get_results(option):
     os.system('python airscraper/app.py ' + option)
